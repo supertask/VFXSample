7f00509d19b4f3d9df5a82a499c344fe009954e3
Tasuku TAKAHASHI <lightfox.task@gmail.com>
2018-11-04 17:34:26 +0900

    init VFX effect.

---
+++ Assets/Tmp/Editor/MyCustomNode.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor.ShaderGraph;
+using System.Reflection;
+
+[Title("Custom", "My Custom Node")]
+public class MyCustomNode  : CodeFunctionNode
+{
+    public MyCustomNode()
+    {
+        name = "My Custom Node";
+    }
+
+    protected override MethodInfo GetFunctionToConvert()
+    {
+        return GetType().GetMethod("MyCustomFunction",
+            BindingFlags.Static | BindingFlags.NonPublic);
+    }
+
+    static string MyCustomFunction(
+        [Slot(0, Binding.None)] DynamicDimensionVector A,
+        [Slot(1, Binding.None)] DynamicDimensionVector B,
+        [Slot(2, Binding.None)] out DynamicDimensionVector Out)
+    {
+        return @"
+{
+    Out = A + B;
+}";
+    }
+}
